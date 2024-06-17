4c2b085f58b12b02e0283a4d057644e8c55020d9
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-06-17 12:12:34 +0545

    Added Navbar

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Options from "./Options";
+import Navbar from "./Navbar"
 
 const Layout = ({ children, setSortingName, sorting }) => {
   return (
     <div className="flex flex-col h-screen justify-start">
+      <Navbar />
       <Options setSortingName={setSortingName} sorting={sorting} />
       <div>{children}</div>
     </div>
---
+++ src/Components/Layout/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div className="flex justify-start text-4xl p-4">
+      <div>
+        Sorting algorithm
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
